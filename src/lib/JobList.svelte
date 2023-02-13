8d7e55034d89fa77483939ab6335183c95e4f66e
Timothee Saisset <timothee.saisset@spacefill.fr>
2023-02-13 13:19:29 +0100

    feat: add reload button

--- src/lib/JobList.svelte
+++ src/lib/JobList.svelte
@@ -30,7 +30,17 @@
 <div>
     {#if jobs.length}
         <div class="divider"></div>
-        <h1>Jobs</h1>
+        <div class='flex justify-between items-center'>
+            <h1>Jobs</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-4"
+                on:click={() => {fetchGetQueryFilter(`pipelines/${pipelineId}/jobs`).then(r => { jobs = r })}} 
+                on:keypress={() => {fetchGetQueryFilter(`pipelines/${pipelineId}/jobs`).then(r => { jobs = r })}} 
+                role='button'
+            >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>
+
+        </div>
         <Table rows={jobs}
             columns={['id', 'stage', 'name', 'status'].map(i => ({
                 key: i,
--- src/lib/JobView.svelte
+++ src/lib/JobView.svelte
@@ -33,7 +33,16 @@
     {#if job}
         <div class="card w-full bg-base-100 shadow-xl my-4">
             <div class="card-body">
-                <h2 class="card-title">Job {job.id}</h2>
+                <div class='flex items-center justify-between'>
+                    <h2 class="card-title">Job {job.id}</h2>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-4"
+                        on:click={() => {fetchJobTrace(job.id).then(r => { jobTrace = new Convert().toHtml(r) })}}
+                        on:keypress={() => {fetchJobTrace(job.id).then(r => { jobTrace = new Convert().toHtml(r) })}}
+                        role='button'
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>
+                </div>
 
                 <div class="stats bg-base-200 shadow">
 
